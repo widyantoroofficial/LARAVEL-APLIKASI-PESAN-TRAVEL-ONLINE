8a5a3e563e96f0cc7cce4e85246f03cb0fa8df3e
adindaagustinmargarets <mikozua45@gmail.com>
2025-02-16 15:30:08 +0700

    test Programming

--- app/Http/Controllers/Auth/Google/GoogleController.php
+++ app/Http/Controllers/Auth/Google/GoogleController.php
@@ -16,24 +16,29 @@ class GoogleController extends Controller
     {
         return Socialite::driver('google')->redirect();
     }
+
     public function handleGoogleCallback()
     {
         $googleUser = Socialite::driver('google')->user();
         $user = User::where('email', $googleUser->getEmail())->first();
 
-        if ($user) {
-            Auth::login($user);
-        } else {
+        if (!$user) {
             $user = User::create([
                 'name' => $googleUser->getName(),
                 'email' => $googleUser->getEmail(),
                 'password' => bcrypt(Str::random(24)),
             ]);
+
+            // Pastikan peran "User" sudah ada di database
+            if (!Role::where('name', 'User')->exists()) {
+                Role::create(['name' => 'User']);
+            }
+
             // Berikan peran "User" secara otomatis
             $user->assignRole('User');
-
-            Auth::login($user);
         }
+
+        Auth::login($user);
 
         return redirect()->route('frontend.landing-page');
     }
