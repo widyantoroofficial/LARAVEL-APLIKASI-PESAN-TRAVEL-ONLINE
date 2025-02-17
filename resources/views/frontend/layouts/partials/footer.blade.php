6ad2528598bd34a430e3d3a08d4d1c782b0d9ccd
adindaagustinmargarets <mikozua45@gmail.com>
2025-02-17 08:22:25 +0700

    test Programming

--- resources/views/frontend/layouts/partials/footer.blade.php
+++ resources/views/frontend/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-gray-800 text-white py-6 mt-auto">
-    <div class="container mx-auto text-center">
-        <div class="flex justify-center space-x-6 mb-4">
+    <div class="container mx-auto text-center px-4">
+        <div class="flex flex-wrap justify-center gap-4 mb-4">
             <a href="#" class="hover:text-blue-400 transition-all duration-300">Tentang Kami</a>
             <a href="#" class="hover:text-blue-400 transition-all duration-300">Kebijakan Privasi</a>
             <a href="#" class="hover:text-blue-400 transition-all duration-300">Syarat dan Ketentuan</a>
